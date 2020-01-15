cb33c1a7386a9f8ab0d4315d7daaa7751840df49
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-15 19:42:26 +0000

    Automatically backed up by Learn

--- app/models/Dog.rb
+++ app/models/Dog.rb
@@ -1,10 +1,13 @@
 class Dog < Sinatra::Base 
   attr_accessor :name 
   
+  @@all = []
+  
   def initialize 
     @id = id
     @name = name 
     @breed = breed
     @age = age
+    @@all << self
   end 
 end 
